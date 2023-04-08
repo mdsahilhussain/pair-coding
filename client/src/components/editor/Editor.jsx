03777440ae922c57339f22e3ae029feebc7cbb8e
mdsahilhussain <mdsahil.hussain7@gmail.com>
2023-04-08 20:44:33 +0530

    destribut into file

--- client/src/components/editor/Editor.jsx
+++ client/src/components/editor/Editor.jsx
@@ -4,10 +4,14 @@ import Editor from "@monaco-editor/react";
 import { PostContext } from "../../context/PostContext";
 import { io } from "socket.io-client";
 import { useLocation, useParams } from "react-router-dom";
+
+const socket = io("http://localhost:5173/");
 const EditorC = () => {
   const { code, setCode, setJoinedUsers, selectedLanguage } =
     useContext(PostContext);
-
+    const location = useLocation();
+    const params = useParams();
+  
   const options = {
     selectOnLineNumbers: true,
   };
@@ -20,49 +24,49 @@ const EditorC = () => {
     markers.forEach((marker) => console.log("onValidate:", marker.message));
   }
 
-  // useEffect(() => {
-  //   const data = {
-  //     name: location.state,
-  //     roomId: params.id,
-  //   };
-  //   socket.emit("setup", { data });
-  // }, []);
+  useEffect(() => {
+    const data = {
+      name: location.state,
+      roomId: params.id,
+    };
+    socket.emit("setup", { data });
+  }, []);
 
-  // useEffect(() => {
-  //   socket.emit("join room", {
-  //     uuid: params.id,
-  //     name: location.state,
-  //   });
-  // }, [params.id]);
+  useEffect(() => {
+    socket.emit("join room", {
+      uuid: params.id,
+      name: location.state,
+    });
+  }, [params.id]);
 
-  // useEffect(() => {
-  //   socket.on("joined", (data) => {
-  //     const { clients } = data;
-  //     const activeUsers = [];
-  //     clients?.filter((client) => {
-  //       if (client.name !== location.state) {
-  //         activeUsers.push(client);
-  //         toast.success(`${client?.name} joined`);
-  //         setJoinedUsers(activeUsers);
-  //       }
-  //     });
-  //   });
-  // });
+  useEffect(() => {
+    socket.on("joined", (data) => {
+      const { clients } = data;
+      const activeUsers = [];
+      clients?.filter((client) => {
+        if (client.name !== location.state) {
+          activeUsers.push(client);
+          toast.success(`${client?.name} joined`);
+          setJoinedUsers(activeUsers);
+        }
+      });
+    });
+  });
 
-  // useEffect(() => {
-  //   const data = {
-  //     uuid: params.id,
-  //     name: location.state,
-  //     code,
-  //   };
-  //   socket.emit("new input", data);
-  // }, [code]);
+  useEffect(() => {
+    const data = {
+      uuid: params.id,
+      name: location.state,
+      code,
+    };
+    socket.emit("new input", data);
+  }, [code]);
 
-  // useEffect(() => {
-  //   socket.on("input recieved", (data) => {
-  //     setCode(data);
-  //   });
-  // }, [socket]);
+  useEffect(() => {
+    socket.on("input recieved", (data) => {
+      setCode(data);
+    });
+  }, [socket]);
 
   return (
     <section className="editor-x___container">
